a0705a931d0ce8d1a55f3a348f50bfa820a8d350
AndreiKolosov <AndreiKolosov@users.noreply.github.com>
2023-02-15 21:39:03 +0200

    feat: complit calculator

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,21 @@
 import './index.scss';
+import { calculatorForm } from '../utils/constants';
 import { initCalculator } from '../components/calculator';
+import { toNumber } from '../utils/utils';
+import { costInput, paymentInput, termInput, dealSum, monthlyPayment as payment } from '../utils/constants';
 
 initCalculator();
+
+calculatorForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formObj = {
+    carPrice: toNumber(costInput.value),
+    firstPay: toNumber(paymentInput.value),
+    term: termInput.value,
+    sumOfDeal: toNumber(dealSum.textContent),
+    monthlyPayment: toNumber(payment.textContent),
+  };
+
+  alert(JSON.stringify(formObj));
+});
